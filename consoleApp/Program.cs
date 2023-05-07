e059563a5fd71cace68e6ab64494e5d204ad900f
“sadikturan” <sadikturan@gmail.com>
2023-05-07 09:31:57 +0300

    Uygulama Değişkenler

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -1,43 +1,20 @@
 ﻿/*
-    Uygulama 1: Bir öğrencinin aşağıdaki bilgileri için gerekli değişkenleri oluşturunuz.
+    C# Veri Tipleri
 
-        Öğrenci adı
-        Öğrenci soyadı
-        Öğrenci ad ve soyad
-        Öğrenci numarası
-        Öğrenci cinsiyet
-        Öğrenci tc kimlik
-        Öğrenci doğum yılı
-        Öğrenci adres bilgisi   
-        Öğrenci yaşı 
+    Value Types: 
+        Tam sayı:            byte, short, int, long
+        Ondalıklı sayılar:   float, double, decimal
+        Diğer Veri Tipleri:  char, boolean, struct
 
-    Uygulama 2: Aşağıdaki ürünlerin toplam bilgisini hesaplayınız.
-    
-        Ürün 1 => 50     TL
-        Ürün 2 => 60.5   TL
-        Ürün 3 => 356.45 TL
+    Reference Types:
+        String, Class, Array, Interface
+
 */
 
-var ogrenciAdi = "Ahmet ";
-var ogrenciSoyadi = "Turan";
-var adSoyad = ogrenciAdi + ogrenciSoyadi;
-var ogrenciNo = "1025";
-var ogrenciTc = "111111";
-var ogrenciDogumYili = 2005;
-var ogrenciYas = 2024 - ogrenciDogumYili;
-var ogrenciAdres = "Kocaeli izmit";
-
-Console.WriteLine(ogrenciAdi);
-Console.WriteLine(ogrenciSoyadi);
-Console.WriteLine(adSoyad);
-Console.WriteLine(ogrenciNo);
-Console.WriteLine(ogrenciTc);
-Console.WriteLine(ogrenciYas);
-Console.WriteLine(ogrenciAdres);
-
-var urun1_fiyat = 50;
-var urun2_fiyat = 60.5;
-var urun3_fiyat = 356.45;
-
-var toplam = urun1_fiyat + urun2_fiyat + urun3_fiyat;
-Console.WriteLine(toplam);
+int sayi = 200;
+decimal kdvOrani = 1.18m;
+char cinsiyet = 'K';
+string cinsiyet2 = "Kadın";
+string urunAdi = "Samsung S23";
+var satisDurumu = false;
+ 
