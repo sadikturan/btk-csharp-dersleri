9e418c9b4679d083e4460f72273a26616f77a997
“sadikturan” <sadikturan@gmail.com>
2023-05-09 14:26:15 +0300

    datetime

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -1,30 +1,25 @@
 ﻿/*
-    String Uygulamaları
+    DateTime
 */
 
-string kursAdi = ".NET 7 ile C# Programlama Dili";
+var simdi = DateTime.Now;
 
-// 1- Kaç karaktere sahiptir ?
-// 2- Hepsini küçük harf yapınız.
-// 3- '.' ile mi başlamaktadır?
-// 4- C# bilgisi hangi konumda bulunmaktadır?
-// 5- String 'C#' bilgisini içeriyor mu?
-// 6- 'Dili' kelimesi yerine 'Dersleri' yazınız. (replace)
- 
- var karakterSayisi = kursAdi.Length;
- Console.WriteLine(karakterSayisi); // 30
+Console.WriteLine(simdi);
+Console.WriteLine(simdi.Year);
+Console.WriteLine(simdi.Month);
+Console.WriteLine(simdi.Day);
+Console.WriteLine(simdi.DayOfWeek);
+Console.WriteLine(simdi.Hour);
+Console.WriteLine(simdi.Minute);
+Console.WriteLine(simdi.Second);
 
- var kucukHarf = kursAdi.ToLower();
- Console.WriteLine(kucukHarf);
+DateTime dt = new DateTime(2022, 6, 10, 14, 30, 45);
+DateTime dt2 = dt.AddYears(1);
+DateTime dt3 = dt.AddHours(2);
 
- var nokta = kursAdi.StartsWith('.');
- Console.WriteLine("string . ile başlıyor: " + nokta);
+Console.WriteLine(dt2.Year);
+Console.WriteLine(dt3.Hour);
 
- var konum = kursAdi.IndexOf("C#");
- Console.WriteLine("konum " + konum);
-
- var iceriyormu = kursAdi.Contains("C#");
- Console.WriteLine("C# içeriyor mu: " + iceriyormu);
-
- var guncellenenString = kursAdi.Replace("Dili","Dersleri");
-Console.WriteLine(guncellenenString);
+var fark = simdi - dt;
+Console.WriteLine(fark.TotalDays);
+Console.WriteLine(fark.TotalMinutes);
