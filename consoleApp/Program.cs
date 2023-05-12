34ee22d150c11b7c2ec5f10d3c7b5b6bac08fee3
“sadikturan” <sadikturan@gmail.com>
2023-05-12 21:41:59 +0300

    referans tipler

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -1,33 +1,10 @@
-﻿// Çok Boyutlu Diziler
+﻿int[] x = {10,20};
+int[] y = x;
 
-// Ali   => 50, 60, 70
-// Ahmet => 60, 80, 90
-// Canan => 50, 70, 30
+Console.WriteLine(x[0]);    // 10
+Console.WriteLine(y[0]);    // 10
 
-// Her öğrencinin not ortalamasını hesaplayınız.
+x[0] = 20;
 
-string[] ogrenciler = {"Ali","Ahmet","Canan"};
-int[,] notlar = new int[3,3];
-
-// ali
-notlar[0,0] = 50;
-notlar[0,1] = 60;
-notlar[0,2] = 70;
-
-// ahmet
-notlar[1,0] = 60;
-notlar[1,1] = 80;
-notlar[1,2] = 90;
-
-// canan
-notlar[2,0] = 50;
-notlar[2,1] = 70;
-notlar[2,2] = 30;
-
-var ortalama_1 = (notlar[0,0] + notlar[0,1] + notlar[0,2]) / 3;
-var ortalama_2 = (notlar[1,0] + notlar[1,1] + notlar[1,2]) / 3;
-var ortalama_3 = (notlar[2,0] + notlar[2,1] + notlar[2,2]) / 3;
-
-Console.WriteLine($"{ogrenciler[0]} isimli öğrencinin not ortalaması: {ortalama_1}");
-Console.WriteLine($"{ogrenciler[1]} isimli öğrencinin not ortalaması: {ortalama_2}");
-Console.WriteLine($"{ogrenciler[2]} isimli öğrencinin not ortalaması: {ortalama_3}");
+Console.WriteLine(x[0]);    // 20
+Console.WriteLine(y[0]);    // 20
