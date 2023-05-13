6f8273d76f5a9bef4c660d29974941d5d7b25142
“sadikturan” <sadikturan@gmail.com>
2023-05-14 00:01:53 +0300

    Aritmetik Operatör Uygulamaları

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -1,16 +1,33 @@
-﻿// Aritmetik Operatörler
+﻿// Aritmetik Operatör Uygulamaları
 
-var a = 10;
-var b = 3;
-var c = 2;
+// a = 10, b = 5, c = 20 ise c - a farkının b katı kaçtır?
 
-// var sonuc = a + b;
-// var sonuc = a - b;
-// var sonuc = a * b;
-// var sonuc = (float)a / (float)b;
-// var sonuc = (a + b) * c;
-// var sonuc = a % b; // 10 % 3
-// var sonuc = 14 % 2;
-var sonuc = ++a;
+// int? a = 50; int b = 20; ise a + b değerini hesaplayınız. (eğer a null ise sonuç nedir?)
 
-Console.WriteLine(sonuc);
+// a=10 b=20 ise a=b--; atamasından sonra a ve b değerleri ne olur?
+
+// Klavyeden girilen bir sayının tek / çift kontrolünü yapınız.
+
+// int a = 10;
+// int b = 5;
+// int c = 20;
+// var sonuc = (c - a) * b;
+
+// int? a = null;
+// int b = 20;
+
+// var sonuc = (a ?? 0) + b;
+// Console.WriteLine(sonuc);
+
+// int a = 10;
+// int b = 20;
+// a = b--;
+
+// Console.WriteLine(a);
+// Console.WriteLine(b);
+
+Console.Write("sayı: ");
+int sayi = int.Parse(Console.ReadLine() ?? "0");
+var sonuc = sayi % 2;
+
+Console.WriteLine(sonuc); // 1 ise satı tek, 0 ise sayı çift
