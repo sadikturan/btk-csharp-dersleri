15d2826dc0d376921a863cc55f2d40d1de1aa607
“sadikturan” <sadikturan@gmail.com>
2023-05-16 08:18:10 +0300

    ygulama: Mantıksal Operatörler

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -1,22 +1,42 @@
-﻿// Mantıksal Operatörler
+﻿// Mantıksal Operatörler Uygulamaları
 
-var a = true;
-var b = true;
-var c = false;
-var d = false;
+// 1- Yaşı 18' den büyük ya da veli izni varsa bir işte çalışabilir durumunu kontrol ediniz.
 
-// 1- Ve - &&
-var sonuc = (a && b); 
-sonuc = (a && c);
-sonuc = (c && d);
+// int yas = 17;
+// bool veli_izni = false;
+// var sonuc = (yas >= 18 || veli_izni) ? "çalışabilir": "çalışamaz";
+// Console.WriteLine(sonuc);
 
-// 2- Veya - ||
-sonuc = (a || b);
-sonuc = (c || b);
-sonuc = (c || d);
+// 2- Ders notu 50-100 arasındaysa geçti değilse kalsın bilgisini yazdırınız.
 
-// 3- Değil - !
-sonuc = !c;
-Console.WriteLine(sonuc);   
+// int not = 40;
+// var sonuc = (not >= 50 && not <= 100) ? "geçti":"kaldı";
+// Console.WriteLine(sonuc);
 
+// 3- Ders ortalaması en az 70 puan ve zayıfı yoksa teşekkür belgesi alabilme durumunu kontrol ediniz.
+
+// int ortalama = 65;
+// int zayif = 0;
+
+// var sonuc = (ortalama >= 70) ? 
+//                 ((zayif == 0) ? "teşekkür alabilir":"zayıfınız olmaması gerekiyor.") :
+//                 "notunuz en az 70 olmalıdır.";
+
+// Console.WriteLine(sonuc);
+
+// 4- İşe girmek için en az önlisans ya da lisans mezunu olma durumunu kontrol ediniz. Sigara kullanmama koşulu.
+
+// string egitim = "lise";
+// bool sigara_icme = false;
+// var sonuc = ((egitim == "lisans" || egitim == "önlisans") && (!sigara_icme)) ? "işe girebilir": "işe giremez";
+// Console.WriteLine(sonuc);
+
+// 5- Uygulamaya giriş kontrolünü username ya da parola için yapalım.
+
+string email = "info@sadikturan.com";
+string username = "sadikturan";
+string password = "12345";
+
+var sonuc = ((email == "info@sadikturan" || username=="sadikturan2") && (password == "12345")) ? "başarılı":"başarısız";
+Console.WriteLine(sonuc);
 
