20e3855b8279dc24a640839d7d73f017686d0eb0
“sadikturan” <sadikturan@gmail.com>
2023-05-19 08:16:49 +0300

    if/else uygulama

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -1,20 +1,54 @@
-﻿// if/else bloğu
+﻿// if/else Uygulamaları
 
-string username = "sadikturan";
-string password = "12345";
+// 1- Kullanıcıya sunulan bir menü içinden seçilecek olan 4 işlem tipine göre hesaplama yapınız.
 
-if (username == "sadikturan") {
+Console.Write("1.sayı: ");
+var sayi1 = Convert.ToInt32(Console.ReadLine());
 
-    if(password == "123456") {
-        Console.WriteLine("Merhaba BTK AKADEMİ");
+Console.Write("2.sayı: ");
+var sayi2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Toplama için: +");
+Console.WriteLine("Çıkarma için: -");
+Console.WriteLine("Çarpma için:  *");
+Console.WriteLine("Bölme için:   /");
+
+Console.Write("Seçiminiz: ");
+var secim = Console.ReadLine();
+
+double sonuc = 0;
+bool ok = true;
+
+if(secim == "+") {
+    sonuc = sayi1 + sayi2;
+} else if(secim == "-") {
+    sonuc = sayi1 - sayi2;
+} else if(secim == "*") {
+    sonuc = sayi1 * sayi2;
+} else if(secim == "/") {
+    
+    if(sayi2 == 0) {
+        ok = false;
+        Console.WriteLine("bölen 0 olamaz.");
     } else {
-        Console.WriteLine("parola yanlış");
+        sonuc = sayi1 / sayi2;
     }
 
+} else {
+    ok = false;
+    Console.WriteLine("yanlış seçim");
 }
-else {
-    Console.WriteLine("username yanlış");
+if(ok) {
+ Console.WriteLine($"işlem sonucu: {sayi1} {secim} {sayi2} = {sonuc}");
 }
 
+// 2- Bir öğrencinin 2 yazılı 1 sözlü notunu alıp hesaplanan ortalamaya göre not aralığına karşılık gelen not bilgisini yazınız.
 
+    // 0  -  24  => 0
+    // 25 -  44  => 1
+    // 45 -  54  => 2
+    // 55 -  69  => 3
+    // 70 -  84  => 4
+    // 85 -  100 => 5
 
+// 3- Girilen 3 sayıdan en büyüğünü bulunuz.
