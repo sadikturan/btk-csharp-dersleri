cd9b2f13e786d332cf979f23096eaf74ec1dc3b6
“sadikturan” <sadikturan@gmail.com>
2023-05-21 22:47:37 +0300

    while

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -1,47 +1,36 @@
-﻿// For Döngüsü Uygulamaları
+﻿// While Döngüsü
 
-// 1- "1-100" arasındaki sayıların toplamını hesaplayınız.
-
-// var toplam = 0;
-
-// for(var i = 1; i <= 100; i++ ) {
-//     if(i % 2 == 1) {
-//         toplam += i;
-//     }
+// for(var i = 0; i < 10; i++) {
+//     Console.WriteLine(i);
 // }
 
-// Console.WriteLine(toplam);
+// var i = 0;
 
-// 2- Klavyeden girilen başlangıç ve bitiş değerleri arasındaki tüm sayıların toplamını hesaplayınız.
-// Console.Write("başlangıç: ");
-// var baslangic = Convert.ToInt32(Console.ReadLine());
-
-// Console.Write("bitiş: ");
-// var bitis = Convert.ToInt32(Console.ReadLine());
-
-// Console.Write("artış: ");
-// var artis = Convert.ToInt32(Console.ReadLine());
-
-// var toplam = 0;
-// for(var i = baslangic; i < bitis; i = i + artis) {
-//     toplam += i;
+// while(i < 10) {
+//     Console.WriteLine(i);
+//     i++;
 // }
-// Console.WriteLine(toplam);
 
-// 3- {"ali","ahmet","can","yelda","seda"} dizindeki tüm elemanları ekrana yazdırın.
+// string[] isimler = {"ali","ahmet","canan"};
 
-// string[] isimler = {"ali","ahmet","yelda","seda","emel"};
-
-// for(var i = 0; i < isimler.Length; i++) {
+// var i = 0;
+// while(i < isimler.Length) {
 //     Console.WriteLine(isimler[i]);
+//     i++;
 // }
 
-// 4- {1,3,4,34,41,56,89} dizisindeki sayılardan hangisileri 3' ün katıdır?
+var secim = "e";
+var sayac = 1;
+var toplam = 0;
 
-int[] sayilar = {1,3,4,36,41,56,89,100};
+while(secim == "e") {
+    Console.Write($"{sayac}. sayı: ");
+    toplam += Convert.ToInt32(Console.ReadLine());
 
-for(var i = 0; i < sayilar.Length; i++) {
-    if(sayilar[i] % 3 == 0) {
-        Console.WriteLine(sayilar[i]);
-    }
+    Console.Write("devam etmek istiyor musunuz? (e/h)");
+    secim = Console.ReadLine();
+
+    sayac++;
 }
+
+Console.WriteLine($"{sayac-1} adet sayının toplamı: {toplam}");
