fde1678c4a13d57c5b9228399dcb19673f24d0b9
“sadikturan” <sadikturan@gmail.com>
2023-05-22 11:07:53 +0300

    break continue

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -1,36 +1,21 @@
-﻿// While Döngüsü
+﻿// break & continue
 
-// for(var i = 0; i < 10; i++) {
-//     Console.WriteLine(i);
+// string isim = "Sadık Turan";
+
+// for(var i = 0; i < isim.Length; i++) {
+//     if(isim[i] == 'ı') 
+//         continue;
+    
+//     Console.WriteLine(isim[i]);
 // }
 
-// var i = 0;
+// Console.WriteLine("döngü bitti");
 
-// while(i < 10) {
-//     Console.WriteLine(i);
-//     i++;
-// }
+int x = 0;
 
-// string[] isimler = {"ali","ahmet","canan"};
-
-// var i = 0;
-// while(i < isimler.Length) {
-//     Console.WriteLine(isimler[i]);
-//     i++;
-// }
-
-var secim = "e";
-var sayac = 1;
-var toplam = 0;
-
-while(secim == "e") {
-    Console.Write($"{sayac}. sayı: ");
-    toplam += Convert.ToInt32(Console.ReadLine());
-
-    Console.Write("devam etmek istiyor musunuz? (e/h)");
-    secim = Console.ReadLine();
-
-    sayac++;
+while(x < 5) {
+    x++;
+    if(x == 3) 
+        continue;
+    Console.WriteLine(x);
 }
-
-Console.WriteLine($"{sayac-1} adet sayının toplamı: {toplam}");
