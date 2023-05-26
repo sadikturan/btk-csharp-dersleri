23438e430aeecb1c5de9fda8e06664815095e902
“sadikturan” <sadikturan@gmail.com>
2023-05-26 14:24:19 +0300

    dosya ve klasörlerla çalışma

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -1,18 +1,33 @@
-﻿// Directory.CreateDirectory("temp");
-// Directory.CreateDirectory("temp/deneme");
-// Directory.CreateDirectory("temp/deneme2");
+﻿// string path = @"C:\temp";
+// string path = "/Users/sadikturan/Desktop/temp/deneme";
 
-// if(Directory.Exists("temp/deneme2")) {
-//     Directory.Delete("temp/deneme2");
-// } else {
-//     Console.WriteLine("silmek istediğiniz klasör yok");
+string rootPath = Directory.GetCurrentDirectory();
+
+// string[] dirs = Directory.GetDirectories(rootPath, "*", SearchOption.AllDirectories);
+
+// foreach(var dir in dirs) {
+//     Console.WriteLine(dir);
 // }
 
+string source_path = "/Users/sadikturan/Desktop/ConsoleApp/img";
+string dest_path = "/Users/sadikturan/Desktop/ConsoleApp/images/";
 
-// string path = @"C:\temp";
-// string path = "/Users/sadikturan/Desktop/temp/deneme";
-string path = Directory.GetCurrentDirectory() + "/temp";
+string[] files = Directory.GetFiles(source_path, "*", SearchOption.AllDirectories);
 
-Console.WriteLine(path);
+foreach(var file in files) {
+    Console.WriteLine(file);
+    Console.WriteLine(Path.GetExtension(file));
+    Console.WriteLine(Path.GetFileNameWithoutExtension(file));
+    Console.WriteLine(Path.GetFileName(file));
 
-Directory.CreateDirectory(path);
+    var info = new FileInfo(file);
+    Console.WriteLine($"{Path.GetFileName(file)}: {info.Length}");
+
+    if(!Directory.Exists(dest_path)) {
+        Directory.CreateDirectory(dest_path);
+    }
+
+    string name = Path.GetRandomFileName() + Path.GetExtension(file);
+    
+    File.Copy(file, $"{dest_path}{name}");
+}
