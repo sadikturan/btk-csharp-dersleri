940c41a69a7e73ff34838d068b67fe7cf5eb5dd7
“sadikturan” <sadikturan@gmail.com>
2023-05-26 11:03:17 +0300

    dosyaya yazma ekleme

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -1,14 +1,23 @@
-﻿// StreamReader sr = File.OpenText("deneme.txt");
-
-// var s = "";
-
-// while((s = sr.ReadLine()) != null) {
-//     Console.WriteLine(s);
+﻿// using(StreamWriter sw = File.CreateText("deneme.txt")) {
+//     sw.WriteLine("Merhaba");
+//     sw.WriteLine("BTK");
+//     sw.WriteLine("Akademi");
 // }
 
+// using(StreamWriter sw = File.AppendText("deneme.txt")) {
+//     sw.WriteLine("Selam");
+//     sw.WriteLine("BTK");
+//     sw.WriteLine("Akademi");
+// }
 
-// string sonuc = File.ReadAllText("deneme.txt");
-string[] sonuc = File.ReadAllLines("deneme.txt");
+File.WriteAllText("deneme.txt", "merhaba");
+File.AppendAllText("deneme.txt", "merhaba");
 
-Console.WriteLine(sonuc[0]);
-Console.WriteLine(sonuc[2]);
+using(StreamReader sr = File.OpenText("deneme.txt")) {
+    var s = "";
+
+    while((s = sr.ReadLine()) != null) {
+        Console.WriteLine(s);
+    }
+}
+
