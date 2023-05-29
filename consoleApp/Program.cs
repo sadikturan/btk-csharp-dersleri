743ee04000acf297b931d29067e621fce0977ca1
“sadikturan” <sadikturan@gmail.com>
2023-05-29 22:17:20 +0300

    constructors

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -6,53 +6,26 @@ namespace ConsoleApp {
 
         static void Main(string[] args) 
         {
-            var soru1 = new Soru() { 
-                SoruMetni = "Hangisi programlama dili değildir?",
-                Secenekler = new string[4] { "Python", "C#", "Java", "Html" },
-                Cevap = "Html" 
-            };
+            var soru1 = new Soru(1, "Hangisi programlama dili değildir?", new string[4] { "Python", "C#", "Java", "Html" }, "Html");
+            var soru2 = new Soru(2, "Hangisi en popüler programlama dilidir?", new string[4] { "Python", "C#", "Java", "Html" }, "C#");
+            var soru3 = new Soru(3, "Hangisi web programlama platformu değildir ?", new string[4] { "Django", "Asp.net", "Spring", "Python" }, "Python");
 
-            var soru2 = new Soru() { 
-                SoruMetni = "Hangisi en popüler programlama dilidir ?",
-                Secenekler = new string[4] { "Python", "C#", "Java", "Html" },
-                Cevap = "C#" 
-            };
-
-            var soru3 = new Soru() { 
-                SoruMetni = "Hangisi web programlama platformu değildir ?",
-                Secenekler = new string[4] { "Django", "Asp.net", "Spring", "Python" },
-                Cevap = "Python" 
-            };
-
-            var sorular = new Soru[] { soru1, soru2, soru3 };
-
-            foreach (var soru in sorular)
-            {
-                Console.WriteLine(soru.SoruMetni);
-                foreach(var secenek in soru.Secenekler) {
-                    Console.WriteLine(secenek);
-                }
-
-                // kullanıcıdan cevap alalım.
-                Console.Write("cevabınız: ");
-                var cevap =  Console.ReadLine();
-
-                if(soru.cevapKontrol(cevap)) {
-                    Console.WriteLine("doğru");
-                } else {
-                    Console.WriteLine("yanlış");
-                }
-            }
         }
     }
 
     class Soru {
-        // properties
+        public Soru(int soruId, string soruMetni, string[] secenekler, string cevap)
+        {
+            this.SoruId = soruId;
+            this.SoruMetni = soruMetni;
+            this.Secenekler = secenekler;
+            this.Cevap = cevap;
+        }
+        public int SoruId { get; set; }
         public string SoruMetni { get; set; }
         public string[] Secenekler { get; set; }
         public string Cevap { get; set; }
         
-        // methods
         public bool cevapKontrol(string cevap) 
         {
             return this.Cevap.ToLower() == cevap.ToLower();
