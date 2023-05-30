049fd0b954907a8c487d08881ce06dd41753bd3f
“sadikturan” <sadikturan@gmail.com>
2023-05-30 11:24:44 +0300

    arraylist

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace ConsoleApp {
 
@@ -6,31 +7,54 @@ namespace ConsoleApp {
 
         static void Main(string[] args) 
         {
-            var soru1 = new Soru(1, "Hangisi programlama dili değildir?", new string[4] { "Python", "C#", "Java", "Html" }, "Html");
-            var soru2 = new Soru(2, "Hangisi en popüler programlama dilidir?", new string[4] { "Python", "C#", "Java", "Html" }, "C#");
-            var soru3 = new Soru(3, "Hangisi web programlama platformu değildir ?", new string[4] { "Django", "Asp.net", "Spring", "Python" }, "Python");
+            // ArrayList
 
+            // non-generic => object
+            // dinamik
+
+            ArrayList liste = new ArrayList();
+
+            liste.Add(10);
+            liste.Add("10");
+            liste.Add("ali");
+            liste.Add(null);
+            liste.Add(true);
+
+            var liste2 = new ArrayList() {
+                5,
+                "ahmet",
+                false,
+                4.5,
+                null
+            };
+
+            int[] sayilar = {10,20,30};
+            
+            liste.AddRange(sayilar);
+
+            var eleman = (int)liste[0];
+            var isim = liste[2].ToString();
+
+            // insert
+            liste.Insert(1, "sadık");
+            liste.InsertRange(2, liste2);
+
+            // remove
+            liste.Remove(10);
+            liste.RemoveAt(2);
+            liste.RemoveRange(2, 3);
+
+            // contains, indexOf
+            Console.WriteLine(liste.Contains(100));
+            Console.WriteLine(liste.IndexOf(10));
+
+            // foreach (var item in liste)
+            // {
+            //     Console.WriteLine(item);
+            // } 
         }
     }
 
-    class Soru {
-        public Soru(int soruId, string soruMetni, string[] secenekler, string cevap)
-        {
-            this.SoruId = soruId;
-            this.SoruMetni = soruMetni;
-            this.Secenekler = secenekler;
-            this.Cevap = cevap;
-        }
-        public int SoruId { get; set; }
-        public string SoruMetni { get; set; }
-        public string[] Secenekler { get; set; }
-        public string Cevap { get; set; }
-        
-        public bool cevapKontrol(string cevap) 
-        {
-            return this.Cevap.ToLower() == cevap.ToLower();
-        }
-    }
-
+    
 }
 
