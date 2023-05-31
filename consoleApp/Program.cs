93fcab0468b20de3a277b523b1ce3457c8c934dc
“sadikturan” <sadikturan@gmail.com>
2023-05-31 06:39:25 +0300

    hata yonetimi nedir

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -7,45 +7,30 @@ namespace ConsoleApp {
 
         static void Main(string[] args) 
         {
-            // Dictionary
+            // Exception
+            // System.FormatException
+            // System.DivideByZeroException
+            // System.NullReferenceException
 
-            // Key-Value => plaka - şehir
+            Console.Write("1. sayı: ");
+            int sayi1 = Convert.ToInt32(Console.ReadLine());
 
-            Dictionary<int,string> plakalar = new Dictionary<int, string>();
+            Console.Write("2. sayı: ");
+            int sayi2 = Convert.ToInt32(Console.ReadLine());
 
-            plakalar.Add(41, "Kocaeli");
-            plakalar.Add(34, "İstanbul");
-            plakalar.Add(53, "Rize");
+            var sonuc = sayi1 / sayi2;
 
-            Dictionary<int,string> sayilar = new Dictionary<int, string>() 
-            {
-                {1, "Bir"},
-                {2, "İki"},
-                {3, "Üç"}
-            };
-
-            Console.WriteLine(plakalar[41]);
-
-            if(plakalar.ContainsKey(34)) {
-                Console.WriteLine(plakalar[34]);
-            }
-
-            foreach(KeyValuePair<int, string> plaka in plakalar)
-            {
-                Console.WriteLine(plaka.Key + " " + plaka.Value);
-            }
-
-            // update
-            sayilar[1] = "one";
-            // sayilar.Remove(1);
-            // sayilar.Clear();
-
-            Console.WriteLine(sayilar[1]);
-
+            Urun urun = null;
+            Console.WriteLine(urun.UrunAdi);
+            
+            // Exception Handling
         }
     }
 
-  
+    class Urun
+    {
+        public string UrunAdi { get; set; }
+    } 
     
 }
 
