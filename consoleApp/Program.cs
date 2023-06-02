d4856d1974397e7d92937006c1c47326a1a6de62
“sadikturan” <sadikturan@gmail.com>
2023-06-02 11:02:10 +0300

    static methods

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -6,31 +6,24 @@ namespace ConsoleApp {
 
         static void Main(string[] args) 
         {
-            var soru1 = new Soru(1, "Hangisi programlama dili değildir?", new string[4] { "Python", "C#", "Java", "Html" }, "Html");
-            var soru2 = new Soru(2, "Hangisi en popüler programlama dilidir?", new string[4] { "Python", "C#", "Java", "Html" }, "C#");
-            var soru3 = new Soru(3, "Hangisi web programlama platformu değildir ?", new string[4] { "Django", "Asp.net", "Spring", "Python" }, "Python");
-
+            var sonuc = HelperMethods.KarakterDuzelt("ölçme ve değerlendirme");
+            Console.WriteLine(sonuc);
         }
     }
 
-    class Soru {
-        public Soru(int soruId, string soruMetni, string[] secenekler, string cevap)
+    class HelperMethods
+    {
+        public static string KarakterDuzelt(string str)
         {
-            this.SoruId = soruId;
-            this.SoruMetni = soruMetni;
-            this.Secenekler = secenekler;
-            this.Cevap = cevap;
-        }
-        public int SoruId { get; set; }
-        public string SoruMetni { get; set; }
-        public string[] Secenekler { get; set; }
-        public string Cevap { get; set; }
-        
-        public bool cevapKontrol(string cevap) 
-        {
-            return this.Cevap.ToLower() == cevap.ToLower();
+            return 
+                str.Replace("ö","o")
+                .Replace("ü","u")
+                .Replace("ı","i")
+                .Replace("ğ","g")
+                .Replace("ç","c")
+                .Replace(" ", "-");
         }
     }
-
+    
 }
 
